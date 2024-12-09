ea1aab6d6e95597414a57335346a93e05c644cfc
VishalR494 <vishalracheti494@gmail.com>
2024-12-09 13:23:01 +0530

    To check working

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vite/plugin-react'
 import react from '@vitejs/plugin-react-swc'
-import { configDefaults } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    global: true,
-    environment: 'jsdom',
-    setipFiles: './src/setupTests.js',
-  }
 })
